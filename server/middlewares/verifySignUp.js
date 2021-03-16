ee3b6923c23cb1d67d474a4599fc6325df31b868
Peter French <peterfrench94@gmail.com>
2021-03-16 18:27:25 +0100

    feat(middlewares): create verifySignUp function

---
+++ server/middlewares/verifySignUp.js
@@ -0,0 +1,36 @@
+const db = require('../db');
+const User = db.User;
+
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+  User.findOne({
+    username: req.body.username
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if (user) {
+      res.status(400).send({ message: 'Failed! Username is already in use!' });
+      return;
+    }
+
+    User.findOne({
+      email: req.body.email
+    }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (user) {
+        res.status(400).send({ message: 'Failed! Email is already in use!' });
+        return;
+      }
+
+      next();
+    });
+  });
+};
+
+module.export = checkDuplicateUsernameOrEmail;
