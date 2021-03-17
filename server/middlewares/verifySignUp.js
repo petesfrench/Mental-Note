cea608b4bdc2d62ad1c897e0729ae2c1881cf2b8
Peter French <peterfrench94@gmail.com>
2021-03-17 12:17:36 +0100

    refactor(verifysignup): change to async try/catch

--- server/middlewares/verifySignUp.js
+++ server/middlewares/verifySignUp.js
@@ -1,36 +1,43 @@
 const db = require('../db');
 const User = db.User;
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  User.findOne({
-    username: req.body.username
-  }).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
+const checkDuplicateUsername = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username
+    });
 
     if (user) {
       res.status(400).send({ message: 'Failed! Username is already in use!' });
       return;
     }
 
-    User.findOne({
-      email: req.body.email
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-
-      if (user) {
-        res.status(400).send({ message: 'Failed! Email is already in use!' });
-        return;
-      }
-
-      next();
-    });
-  });
+    next();
+  } catch (error) {
+    console.error('Error: ', error); // eslint-disable-line no-console
+    res.status(500).send({ message: error });
+  }
 };
 
-module.export = checkDuplicateUsernameOrEmail;
+const checkDuplicateEmail = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email
+    });
+
+    if (user) {
+      res.status(400).send({ message: 'Failed! Email is already in use!' });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error: ', error); // eslint-disable-line no-console
+    res.status(500).send({ message: error });
+  }
+};
+
+module.exports = {
+  checkDuplicateUsername,
+  checkDuplicateEmail
+};
